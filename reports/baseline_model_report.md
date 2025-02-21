1f08ac1c9714e6e7fb28c2d95896b944aa5cdbf0
augustine-emmanuel <emmaustin20@gmail.com>
2025-02-21 02:37:58 +0100

    working script

---
+++ reports/baseline_model_report.md
@@ -0,0 +1,18 @@
+
+# Baseline Model Evaluation
+
+- Model: RandomForestClassifier
+
+- Data Version: v1.6
+
+## Metrics
+
+- F1-Score: 0.84
+
+- Precision: 0.84
+
+- ROC-AUC: 0.59
+
+### Confusion Matrix:
+
+![Confusion matrix](/datav1.6_RandomForestClassifier_confusion_matrix.png)
---
+++ result_comparison.sh
@@ -0,0 +1,41 @@
+#!bin/bash
+file_name="reports/baseline_model_results.csv"
+
+markdown_file="reports/baseline_model_report.md"
+
+best_model=$(sort -t, -nk5 -nr $file_name | head -n 1)
+
+version=$(echo $best_model | cut -d, -f1)
+model=$(echo $best_model | cut -d, -f2)
+precision=$(echo $best_model | cut -d, -f3)
+recall=$(echo $best_model | cut -d, -f4)
+F1_score=$(echo $best_model | cut -d, -f5)
+roc_auc=$(echo $best_model | cut -d, -f6)
+
+if $markdown_file
+  then
+    rm $markdown_file
+  else
+    touch $markdown_file
+fi
+cat <<-EOF > $markdown_file
+# Baseline Model Evaluation
+
+- Model: $model
+
+- Data Version: $version
+
+## Metrics
+
+- F1-Score: $F1_score
+
+- Precision: $precision
+
+- ROC-AUC: $roc_auc
+
+### Confusion Matrix:
+
+![Confusion matrix]($reports/data${version}_${model}_confusion_matrix.png)
+EOF
+
+echo -e "\n!data${version}_${model}_confusion_matrix.png\n\n!data${version}_${model}_class_report.csv" >> .gitignore
